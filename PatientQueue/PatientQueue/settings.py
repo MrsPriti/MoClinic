b79329c8a21b3d48ff141ce7344f50d085fa7fef
Priti Padma Padhi <pritipadmapadhi3@gmail.com>
2024-08-10 21:09:59 +0530

    Patient System Management

--- PatientQueue/PatientQueue/settings.py
+++ PatientQueue/PatientQueue/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-o+bvghpr2yzltyr6!-p(&j#es=qfraj#ql^o5re_8o@_ps5edo"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
